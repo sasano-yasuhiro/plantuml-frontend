35b343d4acb95f1e539a32cb67c4b8ab23a3bed2
sasano yasuhiro <sasanoworkz@gmail.com>
2020-08-25 17:33:20 +0900

    React、scssのモジュール追加

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('Hello World!!!!');
+
+let element = undefined;
+ReactDOM.render(
+  element,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  // ビルド情報の設定
+  devtool: 'inline-source-map', // ソースマップ
+  entry: './src/app.js', // エントリーファイル 
+  output: {
+    path: __dirname + '/public', // 出力ディレクトリ
+    filename: 'bundle.js' // 出力ファイル
+  },
+  // 自動更新の設定
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { url: false }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: ['@babel/plugin-syntax-jsx'],
+          }
+        }
+      },
+    ]
+  },
+};
