5567989d6965d57f315ab56110e901117a7d9139
sasano yasuhiro <sasanoworkz@gmail.com>
2021-07-20 22:53:11 +0900

    AWS Iconのパーサーを追加

---
+++ src/utils/get_aws_components.js
@@ -0,0 +1,24 @@
+import {awsIcons} from './aws_icon' 
+
+export function get_aws_components(){
+  let components = get_icon_files(awsIcons[0].contents)
+  return components
+}
+
+function get_icon_files(contents){
+  let components = {}
+  contents.map((obj)=>{
+    if(obj.type == "directory"){
+      if(!(obj.name in components)){
+        components[obj.name]=[]
+      }
+      components[obj.name].push(get_icon_files(obj.contents))
+    }else if(obj.type == "file"){
+      if(!("file" in components)){
+        components["file"]=[]
+      }
+      components["file"].push(obj.name)
+    }
+  })
+  return components
+}
