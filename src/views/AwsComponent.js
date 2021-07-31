c5f51466fb60b40037a33e332aea186b8eb19a02
sasano yasuhiro <sasanoworkz@gmail.com>
2021-07-31 23:16:15 +0900

    add AWS component list

--- src/views/AwsComponent.js
+++ src/views/AwsComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Tree from './common/tree'
+import {get_aws_components} from '../utils/get_aws_components'
 
 export default class AwsComponents extends React.Component{
   constructor(props){
     super(props);
     this.state={
+      files: get_aws_components(),
     }
   }
   render(){
     return(
-        <div>AwsComponents </div>
+      <Tree items={this.state.files}/>
     )
   }
 }
